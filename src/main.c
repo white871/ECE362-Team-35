ab497e3c139e6e44765c312545fd4d6ea7874bb9
andre1433 <andre143@purdue.edu>
2024-10-30 19:13:40 -0400

    set-up ADC added

--- src/main.c
+++ src/main.c
@@ -18,6 +18,10 @@ const char* username = "mcdonou5";
 #include "stm32f0xx.h"
 #include <stdint.h>
 
+//DECLARATIONS
+void init_input(void);
+void setup_adc(void)
+
 // Returns 2 with 0.9 probability or 4 with 0.1 probability
 int getRandomNumber() {
     return (random(0, 10) < 9) ? 2 : 4;
@@ -28,4 +32,28 @@ void internal_clock();
 int main(void){
     internal_clock();
 
+}
+
+//PROJECT CODE
+
+
+//from lab 4
+void setup_adc(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER &= ~0xC;
+    GPIOA->MODER |= 0xC;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    RCC->CR2 |= RCC_CR2_HSI14ON;
+    while (!(RCC->CR2 & RCC_CR2_HSI14RDY));
+    ADC1->CR |= ADC_CR_ADEN;
+    while (!(ADC1->ISR & ADC_ISR_ADRDY));
+    ADC1->CHSELR = 0;
+    ADC1->CHSELR = (1 << 1); 
+    while (!(ADC1->ISR & ADC_ISR_ADRDY));
+}
+
+void init_input(void){
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    GPIOC->MODER &= ~0xFF00; //clearing PC4-7
+    GPIOC->MODER |= 0x5500; //PC4-7 configured as input
 }
