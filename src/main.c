807ba4b878836132b2ff5d072ea130d7a722bf38
andre1433 <andre143@purdue.edu>
2024-10-30 20:12:25 -0400

    init_i2c added

--- src/main.c
+++ src/main.c
@@ -20,7 +20,10 @@ const char* username = "mcdonou5";
 
 //DECLARATIONS
 void init_input(void);
-void setup_adc(void)
+void setup_adc(void);
+void init_i2c(void);
+void start_i2c(void);
+void stop_i2c(void);
 
 // Returns 2 with 0.9 probability or 4 with 0.1 probability
 int getRandomNumber() {
@@ -31,6 +34,7 @@ void internal_clock();
 
 int main(void){
     internal_clock();
+    enable_ports();
 
 }
 
@@ -40,8 +44,8 @@ int main(void){
 //from lab 4
 void setup_adc(void) {
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-    GPIOA->MODER &= ~0xC;
-    GPIOA->MODER |= 0xC;
+    GPIOA->MODER &= ~0xC; //clear PA1 
+    GPIOA->MODER |= 0xC; //PA1 to analog
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
     RCC->CR2 |= RCC_CR2_HSI14ON;
     while (!(RCC->CR2 & RCC_CR2_HSI14RDY));
@@ -50,4 +54,19 @@ void setup_adc(void) {
     ADC1->CHSELR = 0;
     ADC1->CHSELR = (1 << 1); 
     while (!(ADC1->ISR & ADC_ISR_ADRDY));
+}
+
+void init_i2c(void){
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    GPIOA->MODER &= ~0x3C0000;
+    GPIOA->MODER |= 0x280000; //PA9 and PA10 in alternate function
+    GPIOA->AFR[1] |= 0x440; //PA9 and PA10 to AF4
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+    I2C1->CR1 &= ~I2C_CR1_PE; //disable I2C first
+    I2C1->CR1 |= I2C_CR1_ERRIE; //error interrupts enabled
+    I2C1->CR1 |= I2C_CR1_ANFOFF; //disable analog filter
+    I2C1->TIMINGR |= 0x501F0204; //6 * 8Mhz = 48Mhz clock, 400 kHZ fast mode (supported by EEPROM)
+    I2C1->CR2 &= ~I2C_CR2_ADD10; //7-bit addressing
+    I2C1->CR2 |= I2C_CR2_AUTOEND; //send stop after last NBytes is transferred
+    I2C1->CR1 |= I2C_CR1_PE; //enable I2C
 }
